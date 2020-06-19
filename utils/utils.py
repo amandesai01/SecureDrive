dbecd254da1bc9a6ee736166aae28dd24e4a6883
Aman Desai <amandesai01@gmail.com>
2020-06-19 13:58:50 +0530

    Debugging Removed

--- utils/utils.py
+++ utils/utils.py
@@ -2,9 +2,7 @@ import os
 
 def get_all_filepaths_from_path(basepath):
     f = []
-    print("in1")
     for (dirpath, dirnames, filenames) in os.walk(basepath):
-        print("in2")
         for filename in filenames:
             f.append(dirpath + "/" + filename)
     return f
